6e3f3e69365c05e1fe8c1db5cfa49b11c0474511
charlie24722-code <charlie24722@gmail.com>
2025-10-03 16:24:38 -0600

    nada

--- Semana 3/Clase 03 - 03.py
+++ Semana 3/Clase 03 - 03.py
@@ -1,5 +1,5 @@
-Year = 2025
+year = 2025
 # Son formas de definir variables, es importante hacerlo en ingles
-grad = Year + 5
+grad = year + 5
 
 print(grad)
---
+++ Semana 3/clases 03 - 03.py
@@ -0,0 +1,5 @@
+year = 2025
+
+grad = year + 5
+
+print(grad)
