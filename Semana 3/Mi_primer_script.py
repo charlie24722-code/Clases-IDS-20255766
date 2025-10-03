6974543740a2bcd519c2dc90ead309b18c864d9d
charlie24722-code <charlie24722@gmail.com>
2025-10-03 14:50:02 -0600

    Clase

---
+++ Semana 3/Mi_primer_script.py
@@ -0,0 +1,10 @@
+# Este programa funciona con Python 1. 13. 7
+
+Variable1 = 100
+Variable2 = 150
+Variable3 = Variable1 + Variable2 #Esto no se va a usar
+print(Variable3)
+"""Este es un ejemplo de comentario
+multilinea. Todo lo que esta 
+entre estas comillas dobles,
+no se va a ejecutar como tal"""
