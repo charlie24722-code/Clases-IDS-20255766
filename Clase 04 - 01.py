6fb8fdf782b584df170f9a0e0cfada6c7a0b2963
charlie24722-code <charlie24722@gmail.com>
2025-10-07 11:02:55 -0600

    Clase del martes

---
+++ Clase 04 - 01.py
@@ -0,0 +1,32 @@
+usuario = "Javier"
+
+cantidad_alumnos = 79
+media_edad = 18.6363627
+monto_hope = 1234.56840302
+inversion_evento = 98765.21548
+
+"""print(type(cantidad_alumnos))
+print(type(media_edad)) 
+
+print(type(cantidad_alumnos) is int)  #no es lo mismo "int" que int
+print(type(media_edad) is int)
+
+print("El usuario es", usuario, "y tiene", cantidad_alumnos, "pajaritos en su aula")
+print("y la edad promedio es de", media_edad)
+
+print(f"el usuario es {usuario}")
+print(f"y en su aula con {cantidad_alumnos - 4} pajaritos en su aula")
+print(f"con edad promedio de {media_edad:.1f} años")
+print(f"colectaron ${monto_hope:,.2f} como un donativo.")
+print(f"y la totalidad de gastos fue de ${abs(inversion_evento):,.2f} dolares.")
+
+print(type(usuario) is str)
+
+esta_lloviendo = False
+print(type(esta_lloviendo)is not bool)
+print(type(monto_hope) is not bool)"""
+
+nombre = "Alvin"
+apellido = "Portillo"
+nombre_completo = nombre + " "  + apellido
+print(nombre_completo)
