969a98f1e582d5cc6367e8fcd51ecd01488b1408
charlie24722-code <charlie24722@gmail.com>
2025-10-07 14:58:09 -0600

    Create Clase 04 - 03.py

---
+++ Clase 04 - 03.py
@@ -0,0 +1,20 @@
+
+my_string = "ABCCCDEFGHI" # string, cadena de 5 caracteres
+
+"""letra = int (input("Indique el indice de la letra a mostrar (del 0 al 4):"))
+print(my_string[letra])"""
+
+# print(len(my_string)) 
+# print(my_string[1:5:50]) para subsetting por slices
+
+# print("H" in my_string) # True o False
+# print("O" not in my_string) # True o False
+
+# print("AJP"*8)
+
+# print(min(my_string)) 
+# print(max(my_string))
+
+print(my_string.count("C"))
+
+my_string.
