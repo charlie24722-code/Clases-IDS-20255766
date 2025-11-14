cde05e48f7a4b1fd964d8cad2ca85fdf80621fca
charlie24722-code <charlie24722@gmail.com>
2025-11-14 12:28:59 -0600

    Control 4

--- Control 4/estudiantes.py
+++ Control 4/estudiantes.py
@@ -2,7 +2,7 @@
 def registrar_estudiante(lista_estudiantes):
     """Esta función registra el nombre y carnet de los estudiantes"""
     nombre = input("Nombre: ")
-    numeros = len(lista_estudiantes) + 1 
+    numeros = len(lista_estudiantes) + 1 # para generar automaticamente los carnets
     if numeros < 10:
         carnet = "S00" + str(numeros)
     elif numeros < 100:
@@ -10,16 +10,15 @@ def registrar_estudiante(lista_estudiantes):
     else: 
         carnet = "S" + str(numeros)
     estudiante = {"carnet": carnet,
-                      "nombre": nombre}
-    
+                      "nombre": nombre} # diccionario que guarda nombres y carnets
     lista_estudiantes.append(estudiante)
-    print(f"Estudiante registrado: {nombre} con carnet {carnet}")
+    print(f"Estudiante registrado: {nombre} con carnet {carnet}") # Imprime el nombre y carnet del estudiante para que pueda realizar sus prestamoss
 
 # Creare esta función para mostrar los estudiantes
 def mostrar_estudiantes(lista_estudiantes):
     """Esta función muestra los estudiantes que estan en la lista"""
     if len(lista_estudiantes) == 0:
         print("No hay ningun estudiante registrado")
-        return
+        return # para mostrar que no hay ningun estudiante en la lista 
     else:
         print(lista_estudiantes)
--- Control 4/prestamos.py
+++ Control 4/prestamos.py
@@ -7,38 +7,38 @@ def registrar_prestamo(lista_libros,lista_estudiantes,lista_prestamos):
     for estudiante in lista_estudiantes:
         if estudiante["carnet"] == carnet:
             encontrado = True   
-            print("Este carnet esta registrado")      
+            print("Este carnet esta registrado") # para buscar y encontrar el carnet que ha sido ingresado
     if encontrado == False:
             print("Este carnet no esta registrado")
-            return
+            return # devuelve al inicio si el carnet no ha sido registrado
       
     codigo = input("Ingrese el código del libro a prestrar: ")        
-    libro_prestar = {}
+    libro_prestar = {} # diccionario vacio para registrar información
     for libro in lista_libros:
         if libro["codigo"] == codigo:
-            libro_prestar = libro
+            libro_prestar = libro # inserta la información extraida de la lista_libros despues de verificarse que los codigos sean iguales
     if len(libro_prestar) == 0:
         print("Este código no pertenece a ningun libro")
-        return
+        return # verifica si el codigo es válido
     else:
         print("El libro fue encontrado con éxito")    
     if libro_prestar["disponible"] == False:
         print("Este libro ya ha sido prestado")
-        return
+        return # verifica si el libro ha sido prestado 
     
     fecha = input("Ingrese la fecha del prestamo (ej:dd/mm/aa): ")
     prestamo = {"carnet_estu": carnet,
                 "codigo_libro": codigo,
-                "fecha": fecha}
-    lista_prestamos.append(prestamo)
+                "fecha": fecha} # guarda la información que ha sido registrada anteriormente
+    lista_prestamos.append(prestamo) # añade el diccionario a la lista
     libro_prestar["disponible"] = False
-    print("Prestamo registrado exitosamente")
+    print("Prestamo registrado exitosamente") # cambia el estado de disponibildad para que el libro prestado ya no este disponible
 
 # Creare una función para mostrar prestamos
 def mostrar_prestamos(lista_prestamos):
     """Esta función muestra los prestamos que estan en curso"""
-    if len(lista_prestamos) == 0:
-        print("No hay prestamos en curso")
+    if len(lista_prestamos) == 0: # verifica si hay prestamos
+        print("No hay prestamos en curso") 
         return
     else:
         print(lista_prestamos)
